18ce01101e3cceeca486412b2276e6747eceb820
Adix481 <adisarodey@gmail.com>
2025-11-28 22:01:48 +0530

    Milestone-0 HMS-MAD-1

--- hospital_management/app.py
+++ hospital_management/app.py
@@ -1,3 +1,8 @@
 
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
 app = Flask(__name__) #_name_tells you where the app is located
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hospital.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
